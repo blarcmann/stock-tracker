bfe0396548cc2b4282d3bb111ff4a24d20fe0d6e
yemiOdetola <yemiotola@gmail.com>
2019-05-22 15:40:15 +0100

    stocks: daily increament/decreament

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { StocksService, StockInterface } from './services/stocks.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'stock-tracker';
+  stocks: Array<StockInterface>;
+
+  constructor(service: StocksService) {
+    service.load(['AAPL']).subscribe(stocks => {
+      this.stocks = stocks;
+    })
+  }
+
+}
