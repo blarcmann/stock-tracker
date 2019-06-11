9d09873643ade230c915714b90de92fbd643d31c
blarcmann <yemiotola@outlook.com>
2019-06-11 01:54:35 +0100

    can't remember what I did last

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { StockInterface, StocksService } from 'src/app/services/stocks.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  stocks: Array<StockInterface>;
+  symbols: Array<string>;
 
-  constructor() { }
+  constructor(private service: StocksService) {
+    this.symbols = service.get();
+   }
 
   ngOnInit() {
+    this.service.load(this.symbols).subscribe(
+      stocks => this.stocks = stocks
+    );
   }
 
 }
